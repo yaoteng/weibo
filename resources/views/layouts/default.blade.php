245f963cb15f643447b17a6bcbcd7bacc7bd5506
yaoteng <pangzi.zi@163.com>
2019-04-28 07:42:35 +0800

    用户显示页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title','Weibo App')--laravel 新手入门教程</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
   </head>
   <body>
     @include('layouts._header')
